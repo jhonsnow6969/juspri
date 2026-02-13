c4a1d853b43dddcaa51ba56bdae440c112ece8bb
revanthlol <workofotb@gmail.com>
2026-02-13 16:08:06 +0530

    moved md's to docs folder , addes pwa support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,26 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    }),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 80 },
+      pngquant: { quality: [0.8, 0.9] },
+      svgo: { plugins: [{ name: 'removeViewBox' }] }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
